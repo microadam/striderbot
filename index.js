4ce41acc918751883914bc01429f4b2c72bc3736
Adam Duncan <adam.duncan@clock.co.uk>
2014-03-21 21:04:27 +0000

    Inital Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express')
+  , app = express()
+  , request = require('request')
+
+app.configure(function() {
+  app.use(express.bodyParser())
+})
+
+app.post('/:project', function(req, res) {
+
+  console.log('Payload received:', req.body)
+
+  var project = req.body.payload.project
+    , branch = req.body.payload.branch
+    , repoUrl = process.env.STRIDER_URL + project
+    , success = req.body.payload['test_exitcode'] === 0
+    , messageSuffix = ' on ' + branch + ' - <' + repoUrl + '>'
+    , messagePrefix = 'SUCCESS - Tests have passed successfuly'
+
+  if (!success) {
+    messagePrefix = 'FAILURE - Tests have failed'
+  }
+
+  var data =
+        { channel: '#' + req.params.project
+        , username: 'striderbot'
+        , 'icon_url': 'https://avatars1.githubusercontent.com/u/2199591?s=140'
+        , text: messagePrefix + messageSuffix
+        }
+    , token = process.env.SLACK_TOKEN
+    , url = 'https://clock.slack.com/services/hooks/incoming-webhook?token=' + token
+
+  request.post(
+    { url: url
+    , json: true
+    , body: data
+    }
+  , function(error, response, body) {
+      if (error) {
+        console.log(error)
+        return res.send(500)
+      }
+      console.log('Slack response:', body)
+      res.send(200)
+    }
+  )
+})
+
+var port = process.env.PORT || 6200
+app.listen(port, function() {
+  console.log('Listening on ' + port)
+})
